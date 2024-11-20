28f062454b3d16697212d2e82341a41bf911fc09
imadnanhassan <abulhassan.dev@gmail.com>
2024-11-20 20:54:49 +0600

    student validation in model

--- src/modules/student/student.model.ts
+++ src/modules/student/student.model.ts
@@ -1,55 +1,80 @@
 // student schema
 
 import { model, Schema } from "mongoose";
-import { IStudent } from "./student.interface";
+import { Guardian, IStudent, LocalGuardian } from "./student.interface";
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: {
+    type: String,
+    required: true,
+  },
+  fatherOccupation: {
+    type: String,
+    required: true,
+  },
+  fatherContactNo: {
+    type: String,
+    required: true,
+  },
+  mothersName: {
+    type: String,
+    required: true,
+  },
+  mothersOccupation: {
+    type: String,
+    required: true,
+  },
+  motherContactNo: {
+    type: String,
+    required: true,
+  },
+});
+
+const localGuardianSchema = new Schema<LocalGuardian>({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  address: { type: String, required: true },
+});
 
 const studentSchema = new Schema<IStudent>({
-  id: { type: String },
+  id: { type: String, required: true, unique: true },
   name: { type: String, required: true },
-  email: { type: String, required: true },
-  gender: ["Male", "Female"],
+  email: { type: String, required: true, unique: true },
+  gender: {
+    type: String,
+    enum: {
+      values: ["Male", "Female"],
+      message: "{VALUE} is not supported",
+    },
+    required: true,
+  },
   dateOfBirth: { type: String },
   contactNumber: { type: String, required: true },
   emergencyNumber: { type: String, required: true },
-  bloodGroup: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+  bloodGroup: {
+    type: String,
+    enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    required: true,
+  },
   presentAddress: { type: String, required: true },
   permanentAddress: { type: String, required: true },
   guardian: {
-    fatherName: {
-      type: String,
-      required: true,
-    },
-    fatherOccupation: {
-      type: String,
-      required: true,
-    },
-    fatherContactNo: {
-      type: String,
-      required: true,
-    },
-    mothersName: {
-      type: String,
-      required: true,
-    },
-    mothersOccupation: {
-      type: String,
-      required: true,
-    },
-    motherContactNo: {
-      type: String,
-      required: true,
-    },
+    type: guardianSchema,
+    required: true,
   },
   localGuardian: {
-    name: { type: String, required: true },
-    occupation: { type: String, required: true },
-    contactNo: { type: String, required: true },
-    address: { type: String, required: true },
+    type: localGuardianSchema,
+    required: true,
   },
   profileImage: {
     type: String,
   },
-  isActive: ["active", "blocked"],
+  isActive: {
+    type: String,
+    enum: ["active", "blocked"],
+    default: "active",
+  },
 });
 
 export const StudentModel = model<IStudent>("Student", studentSchema);
